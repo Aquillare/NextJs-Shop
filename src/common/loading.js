0357e6bf66902909ca6641772ee7570bc4992b92
Miguel <migueleduagui@gmail.com>
2022-04-10 22:50:58 -0300

    añadimos componente Loading

---
+++ src/common/loading.js
@@ -0,0 +1,16 @@
+import styles from '@styles/Loading.module.scss';
+
+const Loading = () => {
+    return(
+        <div className={styles['loading_container']}>
+             <div className={styles.loading}>
+
+             </div>
+        <p className={styles['loading_text']}>Loading</p>
+            
+            
+        </div>
+    );
+};
+
+export default Loading
